5866a693e61b315c340e2308d485961fb21763fb
Nguyen Ngoc Khanh <nguyenngockhanh.pbc@gmail.com>
2020-01-08 01:53:49 +0800

    new message

--- guesser/surveydecimation/message/messageImpl.go
+++ guesser/surveydecimation/message/messageImpl.go
@@ -3,41 +3,42 @@ package message
 import "math"
 
 // Message :
+// Store value x as (1-x) to reduce float vanishing
 type Message float64
 
 // FromInt :
 func FromInt(a int, b int) (ratOut Message) {
-	ratOut = Message(float64(a) / float64(b))
+	ratOut = Message(1 - float64(a)/float64(b))
 	return ratOut
 }
 
 // FromFloat :
 func FromFloat(floatIn float64) (ratOut Message) {
-	ratOut = Message(floatIn)
+	ratOut = Message(1 - floatIn)
 	return ratOut
 }
 
 // ToFloat :
 func ToFloat(ratIn Message) (floatOut float64) {
-	floatOut = float64(ratIn)
+	floatOut = float64(1 - ratIn)
 	return floatOut
 }
 
 // IsNaN :
 func IsNaN(ratIn Message) (IsNaN bool) {
-	IsNaN = math.IsNaN(float64(ratIn))
+	IsNaN = math.IsNaN(float64(1 - ratIn))
 	return IsNaN
 }
 
 // Sign :
 func Sign(ratIn Message) (signOut int) {
-	if ratIn > 0 {
+	if 1-ratIn > 0 {
 		signOut = +1
 	}
-	if ratIn < 0 {
+	if 1-ratIn < 0 {
 		signOut = -1
 	}
-	if ratIn == 0 {
+	if 1-ratIn == 0 {
 		signOut = 0
 	}
 	return signOut
@@ -45,23 +46,23 @@ func Sign(ratIn Message) (signOut int) {
 
 // Abs :
 func Abs(ratIn Message) (ratOut Message) {
-	if Sign(ratIn) == 1 {
+	if Sign(1-ratIn) == 1 {
 		ratOut = +ratIn
 	} else {
-		ratOut = -ratIn
+		ratOut = 1 - (-(1 - ratIn))
 	}
 	return ratOut
 }
 
 // Cmp :
 func Cmp(ratIn1 Message, ratIn2 Message) (signOut int) {
-	if ratIn1 > ratIn2 {
+	if 1-ratIn1 > 1-ratIn2 {
 		signOut = +1
 	}
-	if ratIn1 < ratIn2 {
+	if 1-ratIn1 < 1-ratIn2 {
 		signOut = -1
 	}
-	if ratIn1 == ratIn2 {
+	if 1-ratIn1 == 1-ratIn2 {
 		signOut = 0
 	}
 	return signOut
@@ -69,24 +70,24 @@ func Cmp(ratIn1 Message, ratIn2 Message) (signOut int) {
 
 // Add :
 func Add(ratIn1 Message, ratIn2 Message) (ratOut Message) {
-	ratOut = ratIn1 + ratIn2
+	ratOut = 1 - ((1 - ratIn1) + (1 - ratIn2))
 	return ratOut
 }
 
 // Sub :
 func Sub(ratIn1 Message, ratIn2 Message) (ratOut Message) {
-	ratOut = ratIn1 - ratIn2
+	ratOut = 1 - ((1 - ratIn1) - (1 - ratIn2))
 	return ratOut
 }
 
 // Mul :
 func Mul(ratIn1 Message, ratIn2 Message) (ratOut Message) {
-	ratOut = ratIn1 * ratIn2
+	ratOut = 1 - ((1 - ratIn1) * (1 - ratIn2))
 	return ratOut
 }
 
 // Div :
 func Div(ratIn1 Message, ratIn2 Message) (ratOut Message) {
-	ratOut = ratIn1 / ratIn2
+	ratOut = 1 - ((1 - ratIn1) / (1 - ratIn2))
 	return ratOut
 }
